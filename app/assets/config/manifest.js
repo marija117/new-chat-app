173c5994d949cad12daa84c5eb7f934e8fa1c420
stefan <stefanjokajokic@gmail.com>
2019-12-27 14:28:50 +0100

    Make message view.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 //= link_tree ../images
 //= link_directory ../stylesheets .css
--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,23 @@
 class MessageController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
+  
   def index
+    @messages = Message.all
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+   
+    @message.save
+    redirect_to messages_path
+  end
+   
+  private
+    def message_params
+      params.require(:message).permit(:message)
+    end
 end
