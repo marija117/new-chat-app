5ef7ffe4745914f154137749d11fdaf319ca5d09
marija117 <kuveljic117@gmail.com>
2019-12-30 16:35:16 +0100

    Fixed action cable connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_120605) do
+ActiveRecord::Schema.define(version: 2019_12_30_142141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
