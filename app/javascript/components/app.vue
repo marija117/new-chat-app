7a1c8dbb508cff4baee097fa3b93c8610d90541a
Ana Jokic <anajokic10@gmail.com>
2020-01-23 17:34:07 +0100

    Fix some things in app.vue component

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,54 +1,61 @@
 <template>
-  <div id="app">
-    <div class="chat msg-box" data-channel-subscribe='chat' data-room-id="@{{room_id}}">
-      <div class="d-flex justify-content-center">
-        <div v-if="messageArchive">
-          <a href="#" data-previous-archive="@{{from_date}}"></a>
-          
-        </div>
-      </div>
-      <div class="room_messages">
-        <div v-for="message in messages">
-          <div class="chat-message-container">
-            <div class="row no-gutters">
-              <div class="col d-flex">     
-                <div class="message-content flex-grow-1 msg-element">
-                  <p class="mb-1 content" data-message-id="@{{message.id}}">
-                    {{ message }}
-                  </p>
+  <div id="app" v-if="room_id">
+    <div v-if="messageArchive">
+      <div v-if="messages">
+        <div class="chat msg-box" data-channel-subscribe='chat' :data-room-id="room_id">
+          <div class="d-flex justify-content-center">
+            <div v-if="messageArchive">
 
-                  <div class="text-right">
-                    <div v-if="current_user == message.user_id">
-                      <small>
-                        <i class="fa fa-edit pointer" data-message-content="@{{message.message}}" data-message-id="@{{message.id}}"></i>
-                      </small>
-                      |
-                    </div>
-                    <div v-if="isEdited(message)" %>
-                      <small>
-                        Edited
-                      </small>
-                      |
-                    </div>
-                    <small class="edited">
-                      {{ message.created_at }}
-                    </small>
+              <a href="#" :data-previous-archive="from_date">Load older messages</a>
+              
+            </div>
+          </div>
+          <div class="room_messages">
+            <div v-for="message in messages">
+              <div class="chat-message-container">
+                <div class="row no-gutters">
+                  <div class="col d-flex">     
+                    <div class="message-content flex-grow-1 msg-element" v-on:load="loadMessage(message)">
+                      <p class="mb-1 content" :data-message-id="message.id">
+                        {{ message.message }}
+                      </p>
+
+                      <div class="text-right">
+                        <div v-if="current_user == message.user_id">
+                          <small>
+                            <i class="fa fa-edit pointer" :data-message-content="message.message" :data-message-id="message.id"></i>
+                          </small>
+                          |
+                        </div>
+                        <div v-if="isEdited">
+                          <small>
+                            Edited
+                          </small>
+                          |
+                        </div>
+                        <small class="edited">
+                          {{ message.created_at }}
+                        </small>
+                      </div>
+                    </div>  
                   </div>
-                </div>  
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
 
-    <div>
-      <div class="input-group mb-3">
-        <textarea class: 'chat-input message-field' v-model="message"></textarea>
-        <div class="input-group-append">
-          <button class='btn btn-primary chat-input submit-message'>Send</button>
+        <div>
+          <div class="input-group mb-3">
+            <textarea class='chat-input message-field' v-model="message"></textarea>
+            <div class="input-group-append">
+              <button class='btn btn-primary chat-input submit-message'>Send</button>
+            </div>
+            <div v-if="message">
+              <input type="hidden" :value="message.id" class="message_id" %>
+            </div>
+          </div>
         </div>
-        <input type="hidden" value="@{{ message.id }}" class="message_id" %>
       </div>
     </div>
   </div>
@@ -65,10 +72,20 @@ export default {
       messages: [],
       message: null,
       from_date: new Date(),
+      isEdited: false,
     }
   },
   mounted() {
     this.getMessages();
+  },
+  computed: {
+    idEdited(message) {
+      let created_at = new Date(); 
+      let updated_at = new Date(); 
+      
+      console.log("@@@@@@");
+      return updated_at.getTime() > created_at.getTime();
+    }
   },
   methods: {
     getMessages() {
@@ -79,68 +96,26 @@ export default {
         type: "get",
         data: "",
         success: function(data) {
+          console.log(self.messages)
+          self.messages = data.messages;
           self.messageArchive = data.messageArchive;
-          self.archive = data.archive;
+          console.log(self.messages)
         },
         error: function(data) {}
       })
     },
-    idEdited(message) {
+    loadMessage(message) {
       let created_at = new Date(); 
       let updated_at = new Date(); 
       
-      return updated_at.getTime() > created_at.getTime();
+      console.log("@@@@@@");
+      this.isEdited = updated_at.getTime() > created_at.getTime();
     }
   }
 
 }
 </script>
 
-<style scoped>
-.chat-message-container {
-  padding: 5px;
+<style lang="css">
 
-  .message-content {
-    padding: 5px;
-    border: 1px solid rgb(18, 99, 131), 10%;
-    border-radius: 5px;
-    background: lighten(rgb(18, 99, 131), 10%);
-    color: white;
-  }
-}
-
-.hidden {
-  display: none;
-}
-
-.avatar {
-  max-width: 40px;
-  max-height: 40px;
-  border-radius: 50%;
-}
-
-.pointer {
-  cursor: pointer;
-}
-
-.chat {
-  background: lighten(grey, 50%);
-  height: 50vh;
-  border-radius: 5px 5px 0 0;
-  overflow-y: auto;
-}
-
-.chat-input {
-  border-top: 0 none;
-  border-radius: 0 0 5px 5px;
-}
-
-.room-nav-link {
-  border: 1px solid lighten(rgb(121, 204, 236), 40%);
-  background: lighten(rgb(53, 116, 141), 45%);
-
-  & + .room-nav-link {
-    border-top: 0 none;
-  }
-}
 </style>
